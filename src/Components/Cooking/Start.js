a3875822b88c59c32e39f16acd6ddf3470fa6ba6
Aleksandr Jevlentjev <jevlentjev@gmail.com>
2019-09-01 13:29:57 +0300

    finished start , pause and continue buttons

--- src/Components/Cooking/Start.js
+++ src/Components/Cooking/Start.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-const Start = ({ startCooking, state }) => {
+const Start = ({ startCooking, pauseInitiate, unpauseInitiate, state }) => {
 
-    return (
-        <button
-            onClick={startCooking}
-        >Start!</button>
-
-    );
+    if (state.started === false) {
+        return (
+            <button
+                onClick={startCooking}
+            >Start!</button>
+    
+        );
+    } else if (state.started === true && state.paused === false) {
+        return (
+            <button
+                onClick={pauseInitiate}
+            >Pause!</button> 
+        );
+    } else if (state.started === true && state.paused === true) {
+        return (
+            <button
+                onClick={unpauseInitiate}
+            >Continue!</button> 
+        );
+    }
 
 }
 
--- src/Containers/Cooking.js
+++ src/Containers/Cooking.js
@@ -12,45 +12,87 @@ class Cooking extends Component {
         this.state = {
             recipe: this.props.finishedRecipe,
             currentLine: 1,
-            totalLines: this.props.finishedRecipe.length
+            totalLines: this.props.finishedRecipe.length,
+            started: false,
+            paused: false
         }
+    }
+
+    pauseInitiate = () => {
+        this.setState({paused: true});
+        console.log('PAUSE!');
+    }
+
+    unpauseInitiate = () => {
+        this.setState({paused: false});
+        console.log('UNPAUSE!');
+    }
+
+    startBreak = () => { 
+        console.log('Break 30 sec!')
+
+        const cooking = this;
+        let seconds = 0;
+
+        const breakCounter = setInterval(function() {
+
+            if (cooking.state.paused === false) {
+                seconds ++;
+                console.log(seconds);
+            }
+
+            if (seconds >= 15) {
+                console.log('Break finished, now next line!');
+                cooking.startCooking();
+                clearInterval(breakCounter);
+            }
+
+        },1000);
     }
 
     startCooking = () => {
-        console.log('Starting!')
-        const cooking = this;
-        const initialCurrentTime = this.state.recipe[this.state.currentLine - 1].time;
+            console.log('Starting!')
+            const cooking = this;
+            const initialCurrentTime = this.state.recipe[this.state.currentLine - 1].time;
+    
+            this.setState({started: true});
 
-        if (initialCurrentTime) {
-            let seconds = initialCurrentTime * 60;
+            if (initialCurrentTime) {
+                let seconds = initialCurrentTime * 60;
+    
+                const counter = setInterval(function () {
 
-            const counter = setInterval(function () {
-                seconds--;
-                console.log(seconds);
-
-                if (seconds <= 1) {
-                    if (cooking.state.totalLines > cooking.state.currentLine) {
-                        cooking.setState({ currentLine: cooking.state.currentLine + 1 });
-                        console.log(`Line finished, ${cooking.state.currentLine} line now!`);
-                    } else {
-                        console.log('Cooking finished!');
+                    if (cooking.state.paused === false) {
+                        seconds--;
+                        console.log(seconds);
                     }
-
-                    clearInterval(counter);
-                }
-
-            }, 1000);
+    
+                    if (seconds < 1) {
+    
+                        if (cooking.state.totalLines > cooking.state.currentLine) {
+                            cooking.setState({ currentLine: cooking.state.currentLine + 1 });
+                            console.log(`Line finished, ${cooking.state.currentLine} line now!`);
+                            cooking.startBreak();
+                        } else {
+                            console.log('Cooking finished!');
+                        }
+    
+                        clearInterval(counter);
+                    }
+    
+                }, 1000);
+            }
         }
-    }
 
     render() {
+
         return (
             <div className='Cooking-container'>
                 <h1 className='tc'>Cooking</h1>
 
                 <StickyBar />
 
-                <Start state={this.state} startCooking={this.startCooking} />
+                <Start state={this.state} startCooking={this.startCooking} pauseInitiate={this.pauseInitiate} unpauseInitiate ={this.unpauseInitiate}/>
 
                 <PreviousLine />
 
