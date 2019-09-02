e29f9d6f28cf300509bc23a0d241459451d74c78
Aleksandr Jevlentjev <jevlentjev@gmail.com>
2019-09-02 21:36:38 +0300

    line sizes adjusted

--- src/Components/Preparation/Lines.js
+++ src/Components/Preparation/Lines.js
@@ -8,10 +8,10 @@ const Lines = ({ lines, fillRecipe }) => {
     for (let i = 1; i < lines + 1; i++) {
         lineArray.push(
             <div id={i} className='preparation-lines' key={i}>
-                <div className='input-number'>{i}.</div>
-                <input id="food" className='inputs' placeholder="Current product" type="text" onChange={fillRecipe} />
-                <input id="action" className='inputs' placeholder="What to do with it?" type="text" onChange={fillRecipe} />
-                <input id="time" className='inputs' placeholder="Estimated time in minutes" type="number" onChange={fillRecipe} />
+                <span className='input-number'>{i}.</span>
+                <input id="food" className='inputs food-input' placeholder="Current product" type="text" onChange={fillRecipe} />
+                <input id="action" className='inputs' placeholder="What do you do with it?" type="text" onChange={fillRecipe} />
+                <input id="time" className='inputs time-input' placeholder="Estimated time in minutes" type="number" onChange={fillRecipe} />
             </div>
         )
     }
